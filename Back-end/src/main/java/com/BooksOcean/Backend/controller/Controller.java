03a07d6808e79e5aa7e39433301067f8b50e2acd
Moaz2025 <moaz20ahmed@gmail.com>
2023-11-14 10:53:25 +0200

    implement Google Oauth

---
+++ Back-end/src/main/java/com/BooksOcean/Backend/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.BooksOcean.Backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Home!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, Secured!";
+    }
+}

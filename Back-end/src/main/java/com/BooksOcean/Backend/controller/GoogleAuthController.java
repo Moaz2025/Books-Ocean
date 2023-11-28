93d7aa3ac866803a3e55b0f202f2732710545512
hassanAhmed310 <h01552405179@gmail.com>
2023-11-28 23:49:36 +0200

    Modify Google Login

--- Back-end/src/main/java/com/BooksOcean/Backend/controller/GoogleAuthController.java
+++ Back-end/src/main/java/com/BooksOcean/Backend/controller/GoogleAuthController.java
@@ -1,5 +1,6 @@
 package com.BooksOcean.Backend.controller;
 
+import com.BooksOcean.Backend.entity.Admin;
 import com.BooksOcean.Backend.entity.Buyer;
 import com.BooksOcean.Backend.entity.LoginForm;
 import com.BooksOcean.Backend.entity.LoginResponse;
@@ -18,18 +19,22 @@ public class GoogleAuthController {
     private BuyerService buyerService;
     @PostMapping("/google")
     public ResponseEntity<LoginResponse> signUp(@RequestBody Buyer buyer){
+        System.out.println(buyer.getEmail());
         Buyer buyer1 = buyerService.getBuyerByEmail(buyer.getEmail());
         if (buyer1 == null){
             buyer.setPassword(defaultPassword);
             buyerService.createBuyer(buyer);
             buyer1 = buyerService.getBuyerByEmail(buyer.getEmail());
         }
+        LoginResponse loginResponse = new LoginResponse();
         LoginForm loginForm = new LoginForm();
         loginForm.setEmail(buyer1.getEmail());
         loginForm.setPassword(buyer1.getPassword());
         loginForm.setUserType("buyer");
-        LoginController loginController = new LoginController();
-        return loginController.login(loginForm);
-
+        loginResponse.setMessage("Login successfully");
+        loginResponse.setUserType("buyer");
+        loginResponse.setEmail(loginForm.getEmail());
+        loginResponse.setToken("156666"); //need to change
+        return new ResponseEntity<>(loginResponse, HttpStatus.ACCEPTED);
     }
 }

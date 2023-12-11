3b76707095846b1eb0f8383126dd9a463af68e5b
George-Seleim <seleimgeorge@gmail.com>
2023-12-11 18:03:16 +0200

    search bar and select component

--- Front-end/src/pages/home/Home.tsx
+++ Front-end/src/pages/home/Home.tsx
@@ -88,3 +88,5 @@ const Home = () => {
 };
 
 export default Home;
+
+
--- Front-end/src/pages/home/HomeDefault.tsx
+++ Front-end/src/pages/home/HomeDefault.tsx
@@ -1,17 +1,33 @@
 // HomeDefault.tsx
 import React from 'react';
+import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Book } from '../../model/book';
 import SearchBar from '../../components/SearchBar';
-import { Box } from '@mui/material';
 
-const HomeDefault = () => {
+interface HomeDefaultProps {
+  selectedBook: Book | null;
+}
+
+const HomeDefault = ({ selectedBook }: HomeDefaultProps) => {
+  console.log('Selected Book in HomeDefault:', selectedBook);
+
   return (
     <Box>
-      
-      {/* Include SearchBar here */}
       <SearchBar onSearch={(_query, searchBy) => {}} />
-      <p>Home page</p>
+      {selectedBook ? (
+        <Card>
+          <CardMedia component="img" height="140" image={selectedBook.coverImageLink} alt={selectedBook.title} />
+          <CardContent>
+            <Typography variant="h5">{selectedBook.title}</Typography>
+            <Typography variant="subtitle1">Author: {selectedBook.author}</Typography>
+
+          </CardContent>
+        </Card>
+      ) : (
+        <p>No book selected.</p>
+      )}
     </Box>
   );
-}
+};
 
 export default HomeDefault;
